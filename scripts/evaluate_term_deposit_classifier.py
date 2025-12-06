a31511bf9dfff094a571e8842c37511357edb495
Brymahh <eseoghenabraimah@gmail.com>
2025-12-06 09:36:00 -0800

    Updated logic for saving test score

--- scripts/evaluate_term_deposit_classifier.py
+++ scripts/evaluate_term_deposit_classifier.py
@@ -62,10 +62,9 @@ if __name__ == '__main__':
     main()
 =======
 @click.option('--test-data', type=str, help="Path to test data CSV")
-@click.option('--preprocessor', type=str, help="Path to preprocessor pickle object")
-@click.option('--columns-to-drop', type=str, help="Optional: Path to columns to drop from data")
 @click.option('--pipeline-from', type=str, help="Path to the Directory where the pipeline was saved")
 @click.option('--plot-to', type=str, help="Directory to save the plots")
+@click.option('--table-to', type=str, help="Directory to save the score table")
 @click.option('--target-col', type=str, default='target', help="Name of the target/label column")
 @click.option('--seed', type=int, default=522, help="Random seed")
 >>>>>>> 9d6e157 (Added Evaluation File)
