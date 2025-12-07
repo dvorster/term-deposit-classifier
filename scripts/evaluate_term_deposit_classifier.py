219c50f128cbdacac4c748c8763a0fd0b4dc15ee
Brymahh <eseoghenabraimah@gmail.com>
2025-12-06 21:35:32 -0800

    Updated Evaluate File

--- scripts/evaluate_term_deposit_classifier.py
+++ scripts/evaluate_term_deposit_classifier.py
@@ -16,18 +16,12 @@ warnings.filterwarnings("ignore", category=FutureWarning)
 warnings.filterwarnings("ignore", category=UserWarning)
 
 @click.command()
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 58c3dba (Updated scaled to preprocessed Input)
 @click.option('--processed-test-data', type=str, help="Path to scaled test data CSV")
 @click.option('--pipeline-from', type=str, help="Path to the Directory where the pipeline was saved")
 @click.option('--plot-to', type=str, help="Directory to save the plots")
 @click.option('--table-to', type=str, help="Directory to save the score table")
 @click.option('--target-col', type=str, default='target', help="Name of the target/label column")
 def main(processed_test_data, pipeline_from, plot_to, table_to, target_col):
-<<<<<<< HEAD
     '''
     Evaluates the term deposit classifier on the test data and saves the results.
     '''
@@ -65,61 +59,3 @@ def main(processed_test_data, pipeline_from, plot_to, table_to, target_col):
 
 if __name__ == '__main__':
     main()
-=======
-@click.option('--test-data', type=str, help="Path to test data CSV")
-=======
-@click.option('--scaled-test-data', type=str, help="Path to scaled test data CSV")
->>>>>>> 54dec93 (Made changes to Evaluation Logic)
-@click.option('--pipeline-from', type=str, help="Path to the Directory where the pipeline was saved")
-@click.option('--plot-to', type=str, help="Directory to save the plots")
-@click.option('--table-to', type=str, help="Directory to save the score table")
-@click.option('--target-col', type=str, default='target', help="Name of the target/label column")
-<<<<<<< HEAD
-<<<<<<< HEAD
-@click.option('--seed', type=int, default=522, help="Random seed")
->>>>>>> 05a63c2 (Added Evaluation File)
-=======
-def main(test_data, pipeline_from, plot_to, table_to, target_col):
-=======
-def main(scaled_test_data, pipeline_from, plot_to, table_to, target_col):
->>>>>>> 54dec93 (Made changes to Evaluation Logic)
-=======
->>>>>>> 58c3dba (Updated scaled to preprocessed Input)
-    '''
-    Evaluates the term deposit classifier on the test data and saves the results.
-    '''
-    # Read Data
-    test_df = pd.read_csv(processed_test_data)
-    
-    # Load Pipeline    
-    with open(pipeline_from, "rb") as f:
-        pipe = pickle.load(f)   
-
-    # Prepare X and y
-    X_test = test_df.drop(columns=[target_col])
-    y_test = test_df[target_col]
-
-    # Score on Test Data
-    test_score = round(pipe.score(X_test, y_test), 4)
-    test_score_df = pd.DataFrame({'metric':['accuracy'], 'score': [test_score]})
-    
-    # Check if table_to directory exists, if not create it
-    score_path = os.path.join(table_to, "svc_test_score.csv")
-    test_score_df.to_csv(score_path, index=False)
-
-    # Generate and Save Confusion Matrix
-    ConfusionMatrixDisplay.from_estimator(
-        pipe,
-        X_test,
-        y_test,
-        values_format="d"
-    )
-    plt.title("Test Data: Confusion Matrix for SVC model")
-    
-    plot_path = os.path.join(plot_to, "test_svc_confusion_matrix.png")
-    plt.savefig(plot_path)
-    print(f"Confusion matrix saved to {plot_path}")
-
-if __name__ == '__main__':
-    main()
->>>>>>> 9a111bc (Updated logic for saving test score)
